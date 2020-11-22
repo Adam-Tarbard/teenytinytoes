f5979a1656bb72bc2ef598e2cf1335fe4204e16d
Adam Tarbard <adam@tarbard.net>
2020-11-22 16:17:59 +0000

    changed some pictures

---
+++ teenytinytoes/_pgbackup/Assets/js/index_1602529160.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.slider').slider({
+         height: 600
+    });
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+    $('.modal-trigger').leanModal();
+    $('.carousel').carousel({
+      dist: 0,
+      indicators: true
+    });
+    $('.main-carousel').flickity({
+     // options
+     cellAlign: 'left',
+     contain: true,
+    });
+    var $carousel = $('.carousel').flickity();
+
+    $('.button-group').on( 'click', '.button', function() {
+    var index = $(this).index();
+    $carousel.flickity( 'select', index );
+    });
+});
